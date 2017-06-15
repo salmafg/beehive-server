6791afa38580742cabd34421804bc8e4b18d6a45
Salma <salmagalal94@gmail.com>
2017-06-15 15:43:42 +0200

    Added mongodb config

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require('express');
+const compression = require('compression');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const compression = require('compression');
+var config = require('./config');
 
-var userRouter = require('./server/user/userRoutes');
+// var workerUserRouter = require('./server/workerUser/workerUserRoutes');
 
 mongoose.Promise = global.Promise;
-//var db = mongoose.connect(config.dbUrl, config.dbOpts);
+var db = mongoose.connect(config.dbUrl, config.dbOpts);
 
 var app = express();
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', userRouter);
+// app.use('/', workerUserRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -24,6 +25,6 @@ app.use(function(req, res, next) {
 
 var port = process.env.PORT || 3000;
 app.listen(port);
-console.log("Server is listening on port %s", port);
+console.log('Server is listening on port %s', port);
 
 module.exports = app;
---
+++ config/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    dbUrl: process.env.BEEHIVE_DB_URI || 'mongodb://@ds127962.mlab.com:27962/beehive',
+    dbOpts: {
+        user: process.env.BEEHIVE_DB_USER || 'beehiveAdmin',
+        pass: process.env.BEEHIVE_DB_PASS || 'bHive2017'
+    }
+};
