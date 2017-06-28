07d0297812271eaec887a5c794c262fea1066c75
Salma <salmagalal94@gmail.com>
2017-06-28 18:33:27 +0200

    Configured Cors

--- app.js
+++ app.js
@@ -22,7 +22,13 @@ var app = express();
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+var cors_options = {
+  origin: 'http://localhost:4200',
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  methods: ['GET', 'PUT', 'POST', 'DELETE']
+};
+app.use(cors(cors_options));
 
 var MongoStore = require('connect-mongo')(session);
 var store = new MongoStore({
@@ -49,13 +55,6 @@ app.use('/business', businessUserRoutes);
 app.use('/project', projectRoutes);
 app.use('/tutorial', tutorialRoutes);
 app.use('/business', packageRoutes);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
 
 var port = process.env.PORT || 3000;
 app.listen(port);
